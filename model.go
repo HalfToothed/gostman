3c3932c43cba6c9cd2d66f895ab6cd6d025e940c
HalfToothed <abhishekandmails@gmail.com>
2024-12-21 02:42:20 +0530

    textarea

--- model.go
+++ model.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
+	"github.com/charmbracelet/bubbles/textarea"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -10,12 +12,9 @@ import (
 
 var (
 	borderStyle = lipgloss.NewStyle().
-			Padding(0, 0).
-			Border(lipgloss.RoundedBorder()).
-			BorderForeground(lipgloss.Color("62"))
-	allowedDir    = lipgloss.NewStyle().Foreground(lipgloss.Color("70"))
-	disallowedDir = lipgloss.NewStyle().Foreground(lipgloss.Color("173"))
-	commandStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("105"))
+		Padding(0, 0).
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("62"))
 )
 
 func tabBorderWithBottom(left, middle, right string) lipgloss.Border {
@@ -29,11 +28,10 @@ func tabBorderWithBottom(left, middle, right string) lipgloss.Border {
 var (
 	inactiveTabBorder = tabBorderWithBottom("┴", "─", "┴")
 	activeTabBorder   = tabBorderWithBottom("┘", " ", "└")
-	docStyle          = lipgloss.NewStyle().Padding(1, 2, 1, 2)
 	highlightColor    = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
 	inactiveTabStyle  = lipgloss.NewStyle().Border(inactiveTabBorder, true).BorderForeground(highlightColor).Padding(0, 1)
 	activeTabStyle    = inactiveTabStyle.Border(activeTabBorder, true)
-	windowStyle       = lipgloss.NewStyle().BorderForeground(highlightColor).Padding(2, 0).Align(lipgloss.Center).Border(lipgloss.NormalBorder()).UnsetBorderTop()
+	windowStyle       = lipgloss.NewStyle().BorderForeground(highlightColor).Padding(2, 0).Align(lipgloss.Left).Border(lipgloss.NormalBorder()).UnsetBorderTop()
 )
 
 type Model struct {
@@ -43,8 +41,9 @@ type Model struct {
 	height     int
 	inputField textinput.Model
 	tabs       []string
-	tabContent []string
+	tabContent []textarea.Model
 	activeTab  int
+	focused    string
 }
 
 func NewModel() Model {
@@ -52,10 +51,22 @@ func NewModel() Model {
 	m := Model{width: maxWidth}
 	m.lg = lipgloss.DefaultRenderer()
 	m.styles = NewStyles(m.lg)
+	m.tabs = []string{"Body", "Params", "Authorization", "Headers"}
+
 	m.inputField = textinput.New()
+	m.inputField.Placeholder = "URL"
 	m.inputField.Focus()
-	m.tabs = []string{"Lip Gloss", "Blush", "Eye Shadow", "Mascara", "Foundation"}
-	m.tabContent = []string{"Lip Gloss Tab", "Blush Tab", "Eye Shadow Tab", "Mascara Tab", "Foundation Tab"}
+	m.inputField.Cursor.Blink = false
+	m.focused = "inputField"
+
+	// Initialize tab contents
+	for _, tab := range m.tabs {
+		ta := newTextarea()
+		ta.Placeholder = fmt.Sprintf("Write something in %s...", tab)
+		ta.Focus()
+		ta.Cursor.Blink = false
+		m.tabContent = append(m.tabContent, ta)
+	}
 
 	return m
 }
@@ -66,24 +77,43 @@ func (m Model) Init() tea.Cmd {
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		m.width = msg.Width
 		m.height = msg.Height
 	case tea.KeyMsg:
 		switch keypress := msg.String(); keypress {
-		case "ctrl+c", "q":
+		case "ctrl+c":
 			return m, tea.Quit
-		case "right", "l", "n", "tab":
+		case "right":
 			m.activeTab = min(m.activeTab+1, len(m.tabs)-1)
 			return m, nil
-		case "left", "h", "p", "shift+tab":
+		case "left":
 			m.activeTab = max(m.activeTab-1, 0)
 			return m, nil
+
+		case "tab": // Example key to switch focus
+			if m.focused == "inputField" {
+				m.focused = "tabContent"
+			} else {
+				m.focused = "inputField"
+			}
 		}
 	}
 
-	return m, nil
+	m.sizeInputs()
+
+	// Update based on focus
+	if m.focused == "inputField" {
+		m.inputField, cmd = m.inputField.Update(msg)
+	} else if m.focused == "tabContent" {
+		m.tabContent[m.activeTab], cmd = m.tabContent[m.activeTab].Update(msg)
+	}
+
+	return m, cmd
 }
 
 func (m Model) View() string {
@@ -117,19 +147,29 @@ func (m Model) View() string {
 
 	doc.WriteString(row)
 	doc.WriteString("\n")
-	//doc.WriteString(windowStyle.Width((lipgloss.Width(row) - windowStyle.GetHorizontalFrameSize())).Render(m.tabContent[m.activeTab]))
-
-	tabContent := windowStyle.Width(m.width - 44).Height(m.height - 9).Render(m.tabContent[m.activeTab])
+	tabContent := windowStyle.Width(m.width - 60).Height(m.height - 9).Render(m.tabContent[m.activeTab].View())
 	doc.WriteString(tabContent)
 
 	cmdDesc := doc.String()
-	resPanel := borderStyle.Width(40).Height(m.height - 7).Render(titleStyle.Render("Response"))
+	resPanel := borderStyle.Width(m.width - 102).Height(m.height - 7).Render(titleStyle.Render("Response"))
 	mainPanel := lipgloss.JoinHorizontal(lipgloss.Left, cmdDesc, resPanel)
-	cmdInput := borderStyle.Height(1).Width(m.width - 2).Render(m.inputField.View())
-	body := lipgloss.JoinVertical(lipgloss.Top, cmdInput, mainPanel)
+
+	methodInput := borderStyle.Width(15).Height(1).Render(m.inputField.View())
+	cmdInput := borderStyle.Height(1).Width(m.width - 19).Render(m.inputField.View())
+
+	topPanel := lipgloss.JoinHorizontal(lipgloss.Left, methodInput, cmdInput)
+
+	body := lipgloss.JoinVertical(lipgloss.Top, topPanel, mainPanel)
 
 	return m.styles.Base.Render("gostman"+"\n"+body+"\n"+"Ctrl+h to view help,", "Ctrl+c to quit")
 
+}
+
+func (m *Model) sizeInputs() {
+	for i := range m.tabContent {
+		m.tabContent[i].SetWidth(m.width - 65)
+		m.tabContent[i].SetHeight(m.height - 16)
+	}
 }
 
 func max(a, b int) int {
@@ -144,4 +184,22 @@ func min(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func newTextarea() textarea.Model {
+	t := textarea.New()
+	t.Prompt = ""
+	t.Placeholder = "Type something"
+	t.ShowLineNumbers = true
+	t.Cursor.Style = cursorStyle
+	t.FocusedStyle.Placeholder = focusedPlaceholderStyle
+	t.BlurredStyle.Placeholder = placeholderStyle
+	t.FocusedStyle.CursorLine = cursorLineStyle
+	t.FocusedStyle.Base = focusedBorderStyle
+	t.BlurredStyle.Base = blurredBorderStyle
+	t.FocusedStyle.EndOfBuffer = endOfBufferStyle
+	t.BlurredStyle.EndOfBuffer = endOfBufferStyle
+	t.KeyMap.DeleteWordBackward.SetEnabled(false)
+	t.Blur()
+	return t
 }
