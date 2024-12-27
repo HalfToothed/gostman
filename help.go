177b1269d3a1ea566326712366ee943806df123d
HalfToothed <abhishekandmails@gmail.com>
2024-12-28 03:36:46 +0530

    help page updated

--- help.go
+++ help.go
@@ -1,30 +1,71 @@
 package main
 
 import (
+	"strings"
+
+	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
-const shortcutContent = `
-	Tab = move around
-	Shift + Up = Send Request
-	Shift + Left/Right = Change Tabs- Body/Params/Auth/Headers
-	Ctrl + c = Quit gostman
-	`
+const commandsContent = `Tab = Move Around
+shift + up = Send Request
+ctrl + s = Save Request
+ctrl + Arrow Keys = Change Tabs (Body/Param/Header)
+shift + left = Open dashboard
+ctrl + c = Quit`
+
+func createCommandRows() []table.Row {
+	lines := strings.Split(commandsContent, "\n")
+	rows := make([]table.Row, len(lines))
+	for i, line := range lines {
+		cols := strings.Split(line, " = ")
+		rows[i] = table.Row{cols[0], cols[1]}
+	}
+	return rows
+}
 
 type help struct {
-	width       int
-	height      int
-	styles      *Styles
-	returnModel tea.Model
+	width         int
+	height        int
+	styles        *Styles
+	returnModel   tea.Model
+	commandsTable table.Model
 }
 
 func newHelp(width, height int, styles *Styles, returnModel tea.Model) help {
+
+	columns := []table.Column{
+		{Title: "Command", Width: (width / 2) - 13},
+		{Title: "Description", Width: (width / 2) + 7},
+	}
+	commandsTable := table.New(
+		table.WithColumns(columns),
+		table.WithRows(createCommandRows()),
+		table.WithFocused(false),
+		table.WithHeight(height-7),
+	)
+	commandsTable.GotoBottom()
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.HiddenBorder()).
+		Background(lipgloss.Color("62")).
+		Foreground(lipgloss.Color("230")).
+		BorderBottom(false).
+		Bold(true)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("7")).
+		Bold(false).
+		Margin(0, 0, 0, 0).
+		Padding(0, 0, 0, 0)
+	commandsTable.SetStyles(s)
+
 	return help{
-		width:       width,
-		height:      height,
-		styles:      styles,
-		returnModel: returnModel,
+		width:         width,
+		height:        height,
+		styles:        styles,
+		returnModel:   returnModel,
+		commandsTable: commandsTable,
 	}
 }
 
@@ -39,30 +80,25 @@ func (m help) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.WindowSizeMsg:
 		m.width = msg.Width
 		m.height = msg.Height
+		m.commandsTable.SetHeight(m.height - 7)
+		m.commandsTable.SetWidth(m.width - 2)
 
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "esc":
 			return m.returnModel, nil
+		case "ctrl+c":
+			return m, tea.Quit
 		}
 	}
 	return m, nil
 }
 
 func (m help) View() string {
-	title := "Help"
-	var titleStyle = lipgloss.NewStyle().
-		Background(lipgloss.Color("62")).
-		Foreground(lipgloss.Color("230")).
-		Padding(0, 2).
-		MarginLeft((m.width / 2) - len(title) - 1)
 
 	header := m.appBoundaryView("gostman Help Page")
 	body := borderStyle.Width(m.width - 2).Height(m.height - 4).
-		Render(
-			titleStyle.Render(title) + "\n" +
-				headingStyle.Render("Shortcuts") + "\n" +
-				shortcutContent + "\n\n")
+		Render("\n" + m.commandsTable.View() + "\n\n")
 
 	footer := m.appBoundaryView("<ESC> to go back")
 
